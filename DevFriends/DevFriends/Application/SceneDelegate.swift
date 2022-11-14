7314cf3e8f48eaec785c50a1c1af4cd0be2f47f3
이대현 <ej4658@naver.com>
2022-11-14 23:54:56 +0900

    Chore: 프로젝트 초기 설정
    1. info.plist 파일 경로 설정
    2. PodFile 라이브러리 설치
    3. SceneDelegate 오타 수정

--- DevFriends/DevFriends/Application/SceneDelegate.swift
+++ DevFriends/DevFriends/Application/SceneDelegate.swift
@@ -17,9 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
-        window.makeKeyAndVisible()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = MogakcoViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
