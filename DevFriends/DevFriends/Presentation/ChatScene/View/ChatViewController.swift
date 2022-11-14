9b861307cdf887b5b1709d1581f3a115627a3912
jumi-shim <jumi6827@naver.com>
2022-11-14 18:08:25 +0900

    Feat: ChatViewController 생성

---
+++ DevFriends/DevFriends/Presentation/ChatScene/View/ChatViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ChatViewController.swift
+//  DevFriends
+//
+//  Created by 심주미 on 2022/11/14.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
