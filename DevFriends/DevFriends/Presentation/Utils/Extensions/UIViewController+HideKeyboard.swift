0cae5dce92ee9693f107c97caafb1ede50bb065a
SeungWon Yoo <78123636+seungwon-yoo@users.noreply.github.com>
2022-11-15 19:03:34 +0900

    feat: 화면 밖을 터치하면 키보드 내리기

---
+++ DevFriends/DevFriends/Presentation/Utils/Extensions/UIViewController+HideKeyboard.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+HideKeyboard.swift
+//  DevFriends
+//
+//  Created by 유승원 on 2022/11/15.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        tapGestureRecognizer.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
