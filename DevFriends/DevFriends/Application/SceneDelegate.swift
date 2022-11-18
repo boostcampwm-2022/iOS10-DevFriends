81cc998824b9e82f4e0973ca9975618bfbcd213e
SeungWon Yoo <78123636+seungwon-yoo@users.noreply.github.com>
2022-11-18 23:37:41 +0900

    feat: 임시 사용자 구현(로그인 기능 추가 시 삭제할 것)

--- DevFriends/DevFriends/Application/SceneDelegate.swift
+++ DevFriends/DevFriends/Application/SceneDelegate.swift
@@ -8,47 +8,53 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
     let appDIContainer = AppDIContainer()
     var appFlowCoordinator: AppFlowCoordinator?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .white
         
+        // MARK: 로그인을 구현을 안해서 일단 테스트 uid를 저장하고 이를 필요할 때 가져다 쓸 예정(사용자 정보는 어떻게 관리할지 논의해보기!)
+        if UserDefaults.standard.object(forKey: "uid") as? String == nil {
+            UserDefaults.standard.set("YkocW98XPzJAsSDVa5qd", forKey: "uid")
+        }
+        
         let navigationController = UINavigationController()
         window?.rootViewController = navigationController
-        appFlowCoordinator = AppFlowCoordinator(navigationController: navigationController,
-                                                appDIContainer: appDIContainer)
-        appFlowCoordinator?.start()
-        
         window?.makeKeyAndVisible()
+        appFlowCoordinator = AppFlowCoordinator(
+            navigationController: navigationController,
+            appDIContainer: appDIContainer
+        )
+        appFlowCoordinator?.start()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
