8bcec5a87f73a30a502a3368231c9933abae02cf
SeungWon Yoo <78123636+seungwon-yoo@users.noreply.github.com>
2022-11-18 23:30:50 +0900

    feat: 사용자 정보 구조체 생성

---
+++ DevFriends/DevFriends/Domain/Entities/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  DevFriends
+//
+//  Created by 유승원 on 2022/11/18.
+//
+
+import Foundation
+
+struct User {
+    let nickname: String
+    let profileImagePath: String
+    let categories: [String]
+    let groups: [String]
+}
