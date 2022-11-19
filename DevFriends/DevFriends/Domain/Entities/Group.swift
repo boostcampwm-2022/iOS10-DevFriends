79377eba9c3bd0a64e461b1de8bd3987330a9974
SeungWon Yoo <78123636+seungwon-yoo@users.noreply.github.com>
2022-11-19 17:54:14 +0900

    feat: Group 구조체 안에 chatID 프로퍼티 추가

--- DevFriends/DevFriends/Domain/Entities/Group.swift
+++ DevFriends/DevFriends/Domain/Entities/Group.swift
@@ -9,7 +9,8 @@ import Foundation
 import FirebaseFirestoreSwift
 
 struct Group: Codable, Hashable {
-    @DocumentID var uid: String?
+    @DocumentID var uid: String? // TODO: 왜 옵셔널로 해야만 할까?
     let participantIDs: [String]
     let title: String
+    let chatID: String
 }
