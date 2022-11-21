b479e028a4cb9100f8f714c1ebc8c832e353c720
이대현 <ej4658@naver.com>
2022-11-22 00:25:01 +0900

    Add: FetchGroupUseCase 추가

---
+++ DevFriends/DevFriends/Domain/UseCases/FetchGroupUseCase.swift
@@ -0,0 +1,24 @@
+//
+//  FetchGroupUseCase.swift
+//  DevFriends
+//
+//  Created by 심주미 on 2022/11/19.
+//
+
+import Foundation
+
+protocol FetchGroupUseCase {
+    func execute(groupType: GroupType?, location: (latitude: Double, longitude: Double)?) async throws -> [Group]
+}
+
+final class DefaultFetchGroupUseCase: FetchGroupUseCase {
+    let groupRepository: GroupRepository
+    
+    init(groupRepository: GroupRepository) {
+        self.groupRepository = groupRepository
+    }
+    
+    func execute(groupType: GroupType?, location: (latitude: Double, longitude: Double)?) async throws -> [Group] {
+        return try await groupRepository.fetch(groupType: groupType, location: location)
+    }
+}
