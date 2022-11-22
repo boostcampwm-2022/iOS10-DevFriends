553de433477991de035df0d7b8a11c83eccb4e3a
박상현 <shawnpark98@naver.com>
2022-11-22 21:26:26 +0900

    fix: CategoryRepository 커밋

--- DevFriends/DevFriends/Domain/Repositories/CategoryRepository.swift
+++ DevFriends/DevFriends/Domain/Repositories/CategoryRepository.swift
@@ -6,30 +6,7 @@
 //
 
 import Foundation
-import FirebaseFirestore
-import FirebaseFirestoreSwift
 
 protocol CategoryRepository: ContainsFirestore {
     func fetch(_ categoryIds: [String]) async throws -> [Category]
-}
-
-final class DefaultCategoryRepository: CategoryRepository {
-    func fetch(_ categoryIds: [String]) async throws -> [Category] {
-        return try await withThrowingTaskGroup(of: Category.self) { taskGroup in
-            categoryIds.forEach { id in
-                taskGroup.addTask {
-                    try await self.fetchCategory(id)
-                }
-            }
-            
-            return try await taskGroup.reduce(into: []) { partialResult, category in
-                partialResult.append(category)
-            }
-        }
-    }
-    
-    private func fetchCategory(_ id: String) async throws -> Category {
-        let snapshot = try await firestore.collection("Category").document(id).getDocument()
-        return try snapshot.data(as: Category.self)
-    }
 }
