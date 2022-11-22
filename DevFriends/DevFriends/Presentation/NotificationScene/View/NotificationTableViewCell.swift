385e75b8fd4c632a92041c6eda50456ceab14505
SeungWon Yoo <78123636+seungwon-yoo@users.noreply.github.com>
2022-11-23 01:27:12 +0900

    feat: 알림 셀 UI 구현

--- DevFriends/DevFriends/Presentation/NotificationScene/View/NotificationTableViewCell.swift
+++ DevFriends/DevFriends/Presentation/NotificationScene/View/NotificationTableViewCell.swift
@@ -5,10 +5,39 @@
 //  Created by 유승원 on 2022/11/21.
 //
 
+import Combine
 import SnapKit
 import UIKit
 
 final class NotificationTableViewCell: UITableViewCell {
+    lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.frame = CGRect(x: 0.0, y: 0.0, width: 40, height: 40)
+        return imageView
+    }()
+    lazy var subStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.distribution = .fill
+        return stackView
+    }()
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "알림 제목"
+        label.font = .boldSystemFont(ofSize: 14)
+        label.textColor = .black
+        label.sizeToFit()
+        return label
+    }()
+    lazy var secondaryTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "알림 설명"
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .black
+        label.sizeToFit()
+        return label
+    }()
     lazy var acceptButton: UIButton = {
         let button = UIButton()
         var config = UIButton.Configuration.filled()
@@ -19,8 +48,16 @@ final class NotificationTableViewCell: UITableViewCell {
         config.cornerStyle = .capsule
         config.contentInsets = NSDirectionalEdgeInsets(top: 6.0, leading: 18.0, bottom: 6.0, trailing: 18.0)
         button.configuration = config
+        button
+            .publisher(for: .touchUpInside)
+            .sink {
+                self.setButton(isAccepted: true)
+            }
+            .store(in: &cancellables)
         return button
     }()
+    
+    private var cancellables = Set<AnyCancellable>()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -35,44 +72,55 @@ final class NotificationTableViewCell: UITableViewCell {
 extension NotificationTableViewCell: ReusableType {
     static var reuseIdentifier = String(describing: NotificationTableViewCell.self)
     
-    func layout() {}
+    func layout() {
+        self.addSubview(profileImageView)
+        profileImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(18)
+            make.centerY.equalToSuperview()
+            make.height.width.equalTo(40)
+        }
+        self.addSubview(subStackView)
+        subStackView.snp.makeConstraints { make in
+            make.left.equalTo(profileImageView.snp.right).offset(26)
+            make.centerY.equalToSuperview()
+        }
+        subStackView.addArrangedSubview(titleLabel)
+        subStackView.addArrangedSubview(secondaryTitleLabel)
+    }
     
     func updateContent(data: Notification) {
-        var content = defaultContentConfiguration()
-        
-        content.text = data.groupTitle
-        content.textProperties.font = .boldSystemFont(ofSize: 14)
-        content.secondaryTextProperties.font = .systemFont(ofSize: 14)
-        content.textToSecondaryTextVerticalPadding = 10.0
-        content.image = UIImage.profile
-        
+        titleLabel.text = data.groupTitle
+        profileImageView.image = UIImage.profile?.resize(newWidth: 40)
+
         switch data.type {
         case "joinRequest":
             guard let senderNickname = data.senderNickname, let isOK = data.isOK else { return }
-            content.secondaryText = "\(senderNickname)의 참여 요청"
-            setButton(isSelected: isOK)
+            self.secondaryTitleLabel.text = "\(senderNickname)의 참여 요청"
+            self.setButton(isAccepted: isOK)
         case "joinWait":
-            content.secondaryText = "참여 신청이 완료되었습니다."
+            self.secondaryTitleLabel.text = "참여 신청이 완료되었습니다."
         case "joinSuccess":
-            content.secondaryText = "모임에 가입되셨습니다!"
+            self.secondaryTitleLabel.text = "모임에 가입되셨습니다!"
         default:
             break
         }
-        
-        self.contentConfiguration = content
     }
     
-    private func setButton(isSelected status: Bool) {
-        self.addSubview(acceptButton)
+    private func setButton(isAccepted status: Bool) {
+        self.contentView.addSubview(acceptButton)
         self.acceptButton.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
             make.right.equalToSuperview().offset(-20)
         }
-        if isSelected {
+        print(contentView.subviews)
+        if status {
             var config = self.acceptButton.configuration ?? UIButton.Configuration.filled()
-            config.title = "승인됨"
+            var titleAttr = AttributedString.init("승인됨")
+            titleAttr.font = .boldSystemFont(ofSize: 12)
+            config.attributedTitle = titleAttr
             config.baseBackgroundColor = UIColor(red: 0.851, green: 0.851, blue: 0.851, alpha: 1)
             self.acceptButton.configuration = config
+            self.acceptButton.isUserInteractionEnabled = false
         }
     }
 }
