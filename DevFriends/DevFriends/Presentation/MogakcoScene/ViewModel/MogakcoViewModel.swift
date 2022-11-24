de773192bdc7e37caf610b7384f34e9ede5bc412
이대현 <ej4658@naver.com>
2022-11-24 14:45:40 +0900

    fix: develop 병합 수정

--- DevFriends/DevFriends/Presentation/MogakcoScene/ViewModel/MogakcoViewModel.swift
+++ DevFriends/DevFriends/Presentation/MogakcoScene/ViewModel/MogakcoViewModel.swift
@@ -5,11 +5,68 @@
 //  Created by 이대현 on 2022/11/15.
 //
 
-import Foundation
+import Combine
 
-class MogakcoViewModel {
-    // 핀을 선택하면 나오는 컬렉션 뷰 : mogakcoSubView 변수입니다!
-    // 모각코 정보가 담긴 배열을 받아서, CollectionView의 DataSource에 적용해주어야 할 것 같습니다!
-    // 마찬가지로 모각코 정보 배열을 목록 보기 버튼을 눌렀을때 올라오는 모달에도 전달해주어야 할 것 같습니다.
-    // 올라오는 모달은 showMogakcoModal() 함수에서 생성합니다!
+protocol MogakcoViewModelInput {
+    func fetchAllMogakco()
+    func fetchMogakco(location: Location, distance: Double)
+    func nowMogakco(index: Int)
+    func nowMogakcoWithAllList(index: Int, distance: Double)
+}
+
+protocol MogakcoViewModelOutput {
+    var allMogakcosSubject: PassthroughSubject<[Group], Never> { get }
+    var mogakcosSubject: PassthroughSubject<[Group], Never> { get }
+    var nowMogakcoSubject: PassthroughSubject<Group, Never> { get }
+}
+
+protocol MogakcoViewModelType: MogakcoViewModelInput, MogakcoViewModelOutput { }
+
+class MogakcoViewModel: MogakcoViewModelType {
+    var allMogakcosSubject = PassthroughSubject<[Group], Never>()
+    var mogakcosSubject = PassthroughSubject<[Group], Never>()
+    var nowMogakcoSubject = PassthroughSubject<Group, Never>()
+    
+    private var allMogakcoList: [Group] = []
+    private var nowMogakcoList: [Group] = []
+    private var nowMogakco: Group?
+    
+    private let fetchGroupUseCase: FetchGroupUseCase
+    
+    init(fetchGroupUseCase: FetchGroupUseCase) {
+        self.fetchGroupUseCase = fetchGroupUseCase
+    }
+    
+    func fetchAllMogakco() {
+        Task {
+            let groups = try await fetchGroupUseCase
+                .execute(groupType: .mogakco, location: nil, distance: nil)
+            allMogakcoList = groups
+            allMogakcosSubject.send(groups)
+        }
+    }
+    
+    func fetchMogakco(location: Location, distance: Double) {
+        Task {
+            let groups = try await fetchGroupUseCase
+                .execute(groupType: .mogakco, location: location, distance: distance)
+            nowMogakcoList = groups
+            mogakcosSubject.send(groups)
+        }
+    }
+    
+    func nowMogakco(index: Int) {
+        if index < nowMogakcoList.count {
+            nowMogakco = nowMogakcoList[index]
+            nowMogakcoSubject.send(nowMogakcoList[index])
+        }
+    }
+    
+    func nowMogakcoWithAllList(index: Int, distance: Double) {
+        if index < allMogakcoList.count {
+            nowMogakco = allMogakcoList[index]
+            nowMogakcoSubject.send(allMogakcoList[index])
+            fetchMogakco(location: allMogakcoList[index].location, distance: distance)
+        }
+    }
 }
