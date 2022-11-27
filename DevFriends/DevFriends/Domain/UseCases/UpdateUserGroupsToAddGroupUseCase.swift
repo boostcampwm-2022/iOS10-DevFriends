6aca645bd6350a18df198931a6a3c2f8769fceaa
SeungWon Yoo <78123636+seungwon-yoo@users.noreply.github.com>
2022-11-27 11:14:21 +0900

    fix: User가 모임에 승인되었을 때 GroupID가 AppliedGroups -> Groups 되도록 변경

--- DevFriends/DevFriends/Domain/UseCases/UpdateUserGroupsToAddGroupUseCase.swift
+++ DevFriends/DevFriends/Domain/UseCases/UpdateUserGroupsToAddGroupUseCase.swift
@@ -19,11 +19,27 @@ final class DefaultUpdateUserGroupsToAddGroupUseCase: UpdateUserGroupsToAddGroup
     }
     
     func execute(groupID: String, senderID: String) async throws {
-        var user = try await self.userRepository.fetch(uid: senderID)
+        let user = try await self.userRepository.fetch(uid: senderID)
+        let updatedUser = moveAppliedGroupsToGroups(user: user, groupID: groupID)
+        
+        self.userRepository.update(userID: senderID, user: updatedUser)
+    }
+}
+
+extension DefaultUpdateUserGroupsToAddGroupUseCase {
+    private func moveAppliedGroupsToGroups(user: User, groupID: String) -> User {
+        var user = user
+        
+        var tempAppliedGroups = user.appliedGroupIDs
+        guard let index = tempAppliedGroups.firstIndex(of: groupID)
+        else { fatalError("group update logic is strange.") }
+        tempAppliedGroups.remove(at: index)
+        user.appliedGroupIDs = tempAppliedGroups
+        
         var tempGroups = user.groupIDs
         tempGroups.append(groupID)
         user.groupIDs = tempGroups
         
-        self.userRepository.update(userID: senderID, user: user)
+        return user
     }
 }
