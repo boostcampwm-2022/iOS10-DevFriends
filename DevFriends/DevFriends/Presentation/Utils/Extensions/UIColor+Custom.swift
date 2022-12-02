93652cf3aa29103510184827d7aadcfef92bb8f7
SeungWon Yoo <78123636+seungwon-yoo@users.noreply.github.com>
2022-12-02 18:33:27 +0900

    feat: 사용하는 색을 Asset에서 관리 및 UIColor extension에서 활용

--- DevFriends/DevFriends/Presentation/Utils/Extensions/UIColor+Custom.swift
+++ DevFriends/DevFriends/Presentation/Utils/Extensions/UIColor+Custom.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 extension UIColor {
-    static let devFriendsGreen = UIColor(red: 231 / 255, green: 241 / 255, blue: 223 / 255, alpha: 1)
-    static let devFriendsOrange = UIColor(red: 0.992, green: 0.577, blue: 0.277, alpha: 1)
-    static let devFriendsLightGray = UIColor(red: 0.907, green: 0.947, blue: 0.876, alpha: 1)
-    static let devFriendsGray = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1)
+    static let devFriendsGreen = UIColor(named: "DevFriendsGreen")!
+    static let devFriendsOrange = UIColor(named: "DevFriendsOrange")!
+    static let devFriendsLightGray = UIColor(named: "DevFriendsLightGray")!
+    static let devFriendsGray = UIColor(named: "DevFriendsGray")!
+    
 }
